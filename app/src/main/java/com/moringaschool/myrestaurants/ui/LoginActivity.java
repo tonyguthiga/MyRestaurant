a1a8a4563a8418c1aea70709c93ebb6dee2baa63
tonyguthiga <toniguthiga93@gmail.com>
2021-08-11 16:58:04 +0300

    Add user authentification setup

---
+++ app/src/main/java/com/moringaschool/myrestaurants/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.myrestaurants.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.myrestaurants.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
